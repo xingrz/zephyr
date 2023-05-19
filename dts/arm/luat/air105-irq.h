1f35fd5563012fda610f5f1bff35dd4963331ba3
Chen Xingyu <hi@xingrz.me>
2023-05-19 18:27:11 +0800

    soc: arm: luat: Add support for Air105
    Signed-off-by: Chen Xingyu <hi@xingrz.me>

---
+++ dts/arm/luat/air105-irq.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023 Chen Xingyu <hi@xingrz.me>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __AIR105_IRQ_H__
+#define __AIR105_IRQ_H__
+
+#define DMA_IRQ    0
+#define USB_IRQ    1
+#define USBDMA_IRQ 2
+#define LCD_IRQ    3
+#define SCI0_IRQ   4
+#define UART0_IRQ  5
+#define UART1_IRQ  6
+#define SPI0_IRQ   7
+#define CRYPT0_IRQ 8
+#define TIM0_0_IRQ 9
+#define TIM0_1_IRQ 10
+#define TIM0_2_IRQ 11
+#define TIM0_3_IRQ 12
+#define EXTI0_IRQ  13
+#define EXTI1_IRQ  14
+#define EXTI2_IRQ  15
+#define RTC_IRQ    16
+#define SENSOR_IRQ 17
+#define TRNG_IRQ   18
+#define ADC0_IRQ   19
+#define SSC_IRQ    20
+#define TIM0_4_IRQ 21
+#define TIM0_5_IRQ 22
+#define KBD_IRQ    23
+#define MSR_IRQ    24
+#define EXTI3_IRQ  25
+#define SPI1_IRQ   26
+#define SPI2_IRQ   27
+#define SCI2_IRQ   29
+#define UART2_IRQ  32
+#define UART3_IRQ  33
+#define QSPI_IRQ   35
+#define I2C0_IRQ   36
+#define EXTI4_IRQ  37
+#define EXTI5_IRQ  38
+#define TIM0_6_IRQ 39
+#define TIM0_7_IRQ 40
+#define DCMI_IRQ   42
+#define QR_IRQ     46
+#define GPU_IRQ    47
+#define AWD_IRQ    49
+#define DAC_IRQ    50
+#define SPI5_IRQ   51
+
+#endif /* __AIR105_IRQ_H__ */
---
+++ soc/arm/luat/air105/soc.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2023 Chen Xingyu <hi@xingrz.me>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _LUAT_AIR105_SOC_H_
+#define _LUAT_AIR105_SOC_H_
+
+#endif /* _LUAT_AIR105_SOC_H_ */
