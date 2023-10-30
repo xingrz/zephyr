22cd90e271143c7feb772b50e946c0800b438e2a
Chen Xingyu <hi@xingrz.me>
2023-10-30 23:49:05 +0800

    dts: riscv: wch: Add HCLK clock controller node for CH58x

---
+++ include/zephyr/dt-bindings/clock/ch58x-clocks.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 Chen Xingyu <hi@xingrz.me>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DT_CH58X_CLOCKS_H_
+#define ZEPHYR_DT_CH58X_CLOCKS_H_
+
+#define CH58X_CLOCK_TMR0  0 0
+#define CH58X_CLOCK_TMR1  0 1
+#define CH58X_CLOCK_TMR2  0 2
+#define CH58X_CLOCK_TMR3  0 3
+#define CH58X_CLOCK_UART0 0 4
+#define CH58X_CLOCK_UART1 0 5
+#define CH58X_CLOCK_UART2 0 6
+#define CH58X_CLOCK_UART3 0 7
+
+#define CH58X_CLOCK_SPI0 1 0
+#define CH58X_CLOCK_SPI1 1 1
+#define CH58X_CLOCK_PWMX 1 2
+#define CH58X_CLOCK_I2C  1 3
+#define CH58X_CLOCK_USB  1 4
+#define CH58X_CLOCK_USB2 1 5
+#define CH58X_CLOCK_BLE  1 7
+
+#endif /* ZEPHYR_DT_CH58X_CLOCKS_H_ */
